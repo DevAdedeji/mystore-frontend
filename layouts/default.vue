64e58fadcea5e09ac91c98574dfcec7535a62643
DevAdedeji <adedejitewogbade2@gmail.com>
2023-01-05 19:11:29 +0100

    Assets added

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,6 +9,14 @@
 </template>
 
 <script setup>
+const lang = 'en-US';
 
+useHead({
+  title: 'MyStore | E-commerce platform',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  htmlAttrs: { lang },
+  link: [{ rel: 'icon', type: 'image/png', href: 'favicon.ico' }],
+})
 </script>
 
