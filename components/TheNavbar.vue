974a8d5f767abec82803eac09bc0c8a940e4cb2c
DevAdedeji <adedejitewogbade2@gmail.com>
2023-01-12 20:30:31 +0100

    Working on the signin and register page

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -34,26 +34,33 @@
         />
         <button class="bg-primary-color text-white rounded-md py-2 px-5">Search</button>
       </form>
-      <a href="/cart">
-        <div
-          class="border-2 rounded-lg border-primary-color py-2 px-4 flex items-center gap-2"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            style="fill: rgba(27, 146, 244, 1); transform: ; msfilter: "
-          >
-            <path
-              d="M21 4H2v2h2.3l3.28 9a3 3 0 0 0 2.82 2H19v-2h-8.6a1 1 0 0 1-.94-.66L9 13h9.28a2 2 0 0 0 1.92-1.45L22 5.27A1 1 0 0 0 21.27 4 .84.84 0 0 0 21 4zm-2.75 7h-10L6.43 6h13.24z"
-            ></path>
-            <circle cx="10.5" cy="19.5" r="1.5"></circle>
-            <circle cx="16.5" cy="19.5" r="1.5"></circle>
-          </svg>
-          <p>0</p>
+      <div class="flex items-center gap-4">
+        <div class="flex items-center gap-1 font-medium text-primary-color">
+          <a href="/auth/signin">Sign in</a>
+          <p class="text-2xl">|</p>
+          <a href="/auth/signup">Sign Up</a>
         </div>
-      </a>
+        <a href="/cart">
+          <div
+            class="border-2 rounded-lg border-primary-color py-2 px-4 flex items-center gap-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              style="fill: rgba(27, 146, 244, 1); transform: ; msfilter: "
+            >
+              <path
+                d="M21 4H2v2h2.3l3.28 9a3 3 0 0 0 2.82 2H19v-2h-8.6a1 1 0 0 1-.94-.66L9 13h9.28a2 2 0 0 0 1.92-1.45L22 5.27A1 1 0 0 0 21.27 4 .84.84 0 0 0 21 4zm-2.75 7h-10L6.43 6h13.24z"
+              ></path>
+              <circle cx="10.5" cy="19.5" r="1.5"></circle>
+              <circle cx="16.5" cy="19.5" r="1.5"></circle>
+            </svg>
+            <p>0</p>
+          </div>
+        </a>
+      </div>
     </nav>
     <form
       @submit.prevent="searchProduct"
---
+++ pages/auth/signup.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>Sign up</p>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "auth",
+});
+</script>
